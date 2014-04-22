d347160fec23d5cb524bd857a67ba4e241767d65
bojun <bojun@192.168.5.26>
2014-04-22 19:56:23 +0800

    更新订单Service,订单Mapper

--- woxing/dao/src/main/java/com/hdsx/taxi/woxing/dao/ComplaintMapper.java
+++ woxing/dao/src/main/java/com/hdsx/taxi/woxing/dao/ComplaintMapper.java
@@ -1,16 +1,36 @@
 package com.hdsx.taxi.woxing.dao;
 
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.hdsx.taxi.woxing.bean.Complaint;
+import com.hdsx.taxi.woxing.bean.Estimate;
 /**
  * 投诉dao
  * @author cuipengfei
  *
  */
 public interface ComplaintMapper {
+	
 
-	@Insert("insert into db_complaint(id,orderId,type,content,state,passengerMobile,passengerName,complaint_creat_time,citcode,customid) values(#{id},#{orderId},#{type},#{content},#{state},#{passengerMobile},#{passengerName},#{complainDate},#{citycode},#{customid})")
+	/* 查询投诉 */
+	@Select("SELECT * FROM db_complaint WHERE 1=1 AND orderId=#{orderId}")
+	public Complaint getComplaintById(long orderId);
+
+	/* 新建投诉 */
+	@Insert("insert into db_complaint(id,orderId,type,content,state,passengerMobile,passengerName,complaint_creat_time,"
+			+ "citcode,customid) values(#{id},#{orderId},#{type},#{content},#{state},#{passengerMobile},#{passengerName},#{complainDate},#{citycode},#{customid})")
+	public int createComplaint(Complaint complaint);
+
+	/* 修改投诉 */
+	@Update ("UPDATE db_complaint SET orderId=#{orderId},type=#{type},content=#{content},state=#{state},"
+			+ "passengerMobile=#{passengerMobile},passengerName=#{passengerName},complainDate=#{complainDate},customid=#{customid},citycode=#{citycode}")
 	public int insert(Complaint complaint);
+
+	/* 删除投诉 */
+	@Delete("DELETE FROM db_complaint WHERE orderId = #{orderId}")
+	public int deleteEstimate(long complaintId);
 }
--- woxing/dao/src/main/java/com/hdsx/taxi/woxing/dao/OrderMapper.java
+++ woxing/dao/src/main/java/com/hdsx/taxi/woxing/dao/OrderMapper.java
@@ -2,15 +2,62 @@ package com.hdsx.taxi.woxing.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
+import com.hdsx.taxi.woxing.bean.Estimate;
 import com.hdsx.taxi.woxing.bean.Order;
 
 public interface OrderMapper {
-	@Select("select * from orderaa")
-	public List<Order> getlist();
 
-	@Insert("insert into orderaa (id,operationid,name) values(#{id},#{operationid},#{name})")
+	
+	/* 查询订单 */
+	@Select("SELECT * FROM db_order WHERE 1=1 AND orderId=#{orderId}")
+	public Order getOrderById(long orderId);
+	
+	/* 查询历史订单 */
+	@Select("SELECT * FROM db_order WHERE 1=1 AND orderId=#{orderId}")
+	public List getHistoryOrderByCustomId(String customid);
+
+	/* 查询预约单*/
+	@Select("SELECT * FROM db_order WHERE 1=1 AND orderId=#{orderId}")
+	public List getReservationOrder(String customid);
+
+	
+	/* 新建订单 */
+	@Insert("INSERT INTO db_order (result,citycode,useriphone,paytype,fee,state,"
+			+ "orderId,customid,nickName,sex,getOnTime,lastReplTime,contractTaxi,"
+			+ "vipMark,reservation,takeTaxiType,serverLevel,firstChoiceCompany,"
+			+ "personCount,getOnLon,getOnLat,getOffLon,getOffLat,getOnPlaceName,"
+			+ "getOffPlaceName,notes,motorcycleType,anotherCellPhoneNo,specialRequirements,"
+			+ "orderCreateTime,cityResponse2CenterTime,taxiResponse2CityTime)"
+			+ " VALUES (#{result}, #{citycode}, #{useriphone}, #{paytype}, #{fee}, #{state},"
+			+ " #{orderId}, #{customid}, #{nickName}, #{sex}, #{getOnTime}, #{lastReplTime},"
+			+ " #{contractTaxi}, #{vipMark}, #{reservation}, #{takeTaxiType}, #{serverLevel},"
+			+ " #{firstChoiceCompany}, #{personCount}, #{getOnLon}, #{getOnLat}, #{getOffLon},"
+			+ " #{getOffLat}, #{getOnPlaceName}, #{getOffPlaceName}, #{notes}, #{motorcycleType}, #{anotherCellPhoneNo},"
+			+ " #{specialRequirements}, #{orderCreateTime}, #{cityResponse2CenterTime}, #{taxiResponse2CityTime})")
 	public int insert(Order order);
+
+	/* 修改订单 */
+	@Update("UPDATE db_order SET result=#{result},citycode=#{citycode},useriphone=#{useriphone},paytype=#{paytype},"
+			+ "fee=#{fee},state=#{state},orderId=#{orderId},customid=#{customid},nickName=#{nickName},sex=#{sex},"
+			+ "getOnTime=#{getOnTime},lastReplTime=#{lastReplTime},contractTaxi=#{contractTaxi},vipMark=#{vipMark},"
+			+ "reservation=#{reservation},takeTaxiType=#{takeTaxiType},serverLevel=#{serverLevel},firstChoiceCompany=#{firstChoiceCompany},"
+			+ "personCount=#{personCount},getOnLon=#{getOnLon},getOnLat=#{getOnLat},getOffLon=#{getOffLon},getOffLat=#{getOffLat},"
+			+ "getOnPlaceName=#{getOnPlaceName},getOffPlaceName=#{getOffPlaceName},notes=#{notes},motorcycleType=#{motorcycleType},"
+			+ "anotherCellPhoneNo=#{anotherCellPhoneNo},specialRequirements=#{specialRequirements},orderCreateTime=#{orderCreateTime},"
+			+ "cityResponse2CenterTime=#{cityResponse2CenterTime},taxiResponse2CityTime=#{taxiResponse2CityTime}")
+	public int updateOrder(Order order);
+	
+	/*取消订单*/
+	@Update("UPDATE db_order SET state=1 where orderId=#{orderId}")
+	public int cancelOrder(long orderId);
+	
+	
+	/* 删除订单 */
+	@Delete("DELETE FROM db_order WHERE orderId = #{orderId}")
+	public int deleteOrder(long orderId);
 }
