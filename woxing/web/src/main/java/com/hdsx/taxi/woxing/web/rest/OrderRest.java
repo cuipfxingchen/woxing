876f2a3ccc602523f7310f32ed4af0b3fe8560d1
bojun <bojun@192.168.5.26>
2014-04-22 19:55:46 +0800

    更新订单Rest，订单Service,订单Mapper

--- woxing/web/src/main/java/com/hdsx/taxi/woxing/web/rest/OrderRest.java
+++ woxing/web/src/main/java/com/hdsx/taxi/woxing/web/rest/OrderRest.java
@@ -1,10 +1,14 @@
 package com.hdsx.taxi.woxing.web.rest;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import com.google.inject.Inject;
+import com.hdsx.taxi.woxing.bean.CarInfo;
 import com.hdsx.taxi.woxing.bean.Order;
 import com.hdsx.taxi.woxing.dao.OrderMapper;
 import com.hdsx.taxi.woxing.order.IOrderService;
@@ -18,8 +22,6 @@ public class OrderRest {
 	@Inject
 	IOrderService orderservice;
 
-	
-	
 	/**
 	 * 提交订单
 	 * @param order
@@ -30,7 +32,172 @@ public class OrderRest {
 	@Produces("application/json;charset=UTF-8")
 	public RestBean submit(Order order) {
 		RestBean<Integer> r = new RestBean<>();
+		String success="成功";
+		String fail = "失败";
+		boolean operResult=true;
+		//*业务逻辑开始
 		r.setResult(orderservice.submit(order));
+		
+		
+		//业务逻辑结束
+		if(operResult){
+			r.setState(RestBean.SUCESSCODE);
+			r.setMsg(success);
+		}else{
+			r.setState(RestBean.FAILCODE);
+			r.setMsg(fail);
+		}
+		
 		return r;
 	}
+	
+	
+	/**
+	 * 查询历史订单
+	 * 
+	 * @param customid
+	 * @return
+	 */
+	@Path("/2")
+	@GET
+	@Produces("application/json;charset=UTF-8")
+	public RestBean getHistoryOrder(String customid){
+		RestBean<List<Order>> r = new RestBean<>();
+		String success = "成功";
+		String fail = "失败";
+		boolean operResult = false;
+		//业务逻辑开始
+		r.setResult(orderservice.getHistoryOrder(customid));
+		operResult = true;
+
+		//业务逻辑结束
+		if (operResult) {
+			r.setState(RestBean.SUCESSCODE);
+			r.setMsg(success);
+		} else {
+			r.setState(RestBean.FAILCODE);
+			r.setMsg(fail);
+		}
+		return r;
+	} 
+
+	/**
+	 * 查询预约订单
+	 * 
+	 * @param customid
+	 * @return
+	 */
+	@Path("/3")
+	@GET
+	@Produces("application/json;charset=UTF-8")
+	public RestBean getReservationOrder(String customid){
+		RestBean<List<Order>> r = new RestBean<>();
+		String success = "成功";
+		String fail = "失败";
+		boolean operResult = false;
+		//业务逻辑开始
+		//r.setResult();
+		r.setResult(orderservice.getReservationOrder(customid));
+
+		operResult = true;
+
+		//业务逻辑结束
+		if (operResult) {
+			r.setState(RestBean.SUCESSCODE);
+			r.setMsg(success);
+		} else {
+			r.setState(RestBean.FAILCODE);
+			r.setMsg(fail);
+		}
+		return r;
+	}
+
+	/**
+	 * 取消订单
+	 * 
+	 * @param orderid
+	 * @return
+	 */
+	@Path("/4")
+	@GET
+	@Produces("application/json;charset=UTF-8")
+	public RestBean cancelOrder(long orderid){
+		RestBean<Boolean> r = new RestBean<>();
+		String success = "成功";
+		String fail = "失败";
+		boolean operResult = false;
+		//业务逻辑开始
+		r.setResult(new Boolean(orderservice.cancelOrder(orderid) ));
+		operResult = true;
+
+		//业务逻辑结束
+		if (operResult) {
+			r.setState(RestBean.SUCESSCODE);
+			r.setMsg(success);
+		} else {
+			r.setState(RestBean.FAILCODE);
+			r.setMsg(fail);
+		}
+		return r;
+	}
+
+	/**
+	 * 通过订单查询车辆信息
+	 * 
+	 * @param orderid
+	 * @return
+	 */
+	@Path("/5")
+	@GET
+	@Produces("application/json;charset=UTF-8")
+	public RestBean queryCarInfoByOrder(long orderid){
+		RestBean<CarInfo> r = new RestBean<>();
+		String success = "成功";
+		String fail = "失败";
+		boolean operResult = false;
+		//业务逻辑开始
+		r.setResult( orderservice.queryCarInfoByOrder(orderid));
+		operResult = true;
+
+		//业务逻辑结束
+		if (operResult) {
+			r.setState(RestBean.SUCESSCODE);
+			r.setMsg(success);
+		} else {
+			r.setState(RestBean.FAILCODE);
+			r.setMsg(fail);
+		}
+		return r;
+	}
+
+	
+	/**
+	 * 更新订单，包括状态
+	 * @param order
+	 * @return
+	 */
+	@Path("/6")
+	@GET
+	@Produces("application/json;charset=UTF-8")
+	public RestBean update(Order order){		
+		RestBean<Boolean> r = new RestBean<>();
+		String success = "成功";
+		String fail = "失败";
+		boolean operResult = false;
+		//业务逻辑开始
+		r.setResult(new Boolean(orderservice.update(order)));
+		operResult = true;
+
+		//业务逻辑结束
+		if (operResult) {
+			r.setState(RestBean.SUCESSCODE);
+			r.setMsg(success);
+		} else {
+			r.setState(RestBean.FAILCODE);
+			r.setMsg(fail);
+		}
+		return r;
+		
+	}
+
 }
