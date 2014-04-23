cc6f27897913b03badd7737bbea7ee1596888085
wangx <wangxiri@qq.com>
2014-04-23 15:05:20 +0800

    增加订单号更新通知消息

---
+++ woxing/mqutil/src/main/java/com/hdsx/taxi/woxing/mqutil/message/order/MQMsg1008.java
@@ -0,0 +1,38 @@
+package com.hdsx.taxi.woxing.mqutil.message.order;
+
+import javax.jms.BytesMessage;
+import javax.jms.JMSException;
+
+import com.hdsx.taxi.woxing.mqutil.message.MQAbsMsg;
+
+/**
+ * 2.11 更新订单id通知（0x1008） 字段 数据类型 说明 原来订单id Long 新订单id Long
+ * 
+ * @author Steven
+ * 
+ */
+public class MQMsg1008 extends MQAbsMsg {
+
+	long oldid, newid;
+
+	@Override
+	protected short getMessageId() {
+		// TODO Auto-generated method stub
+		return 0x1008;
+	}
+
+	@Override
+	protected void decodebody(BytesMessage msg) throws JMSException {
+		this.oldid = msg.readLong();
+		this.newid = msg.readLong();
+
+	}
+
+	@Override
+	protected BytesMessage encodebody(BytesMessage msg) throws JMSException {
+		msg.writeLong(oldid);
+		msg.writeLong(newid);
+		return msg;
+	}
+
+}
